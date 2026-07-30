17293852499ef2cacfa932981b7ec162e968f028
Anuj kumar <anujkumar263@gmail.com>
2026-07-31 01:48:05 +0530

    update

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
@@ -4,5 +4,7 @@ public class WelcomeToJava {
     public static void main(String[] args) {
         System.out.println("Welcome to java Track");
         System.out.println("I already know how to built program");
+        System.out.println("Now i am learning how to express that logic using java");
+        System.out.println("Anuj kumar");
     }
 }
