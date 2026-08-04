592803a2ab0c0f5b50f87eb22309c38ebbe5a2df
Anuj Kumar <anujkumar263@gmail.com>
2026-08-04 14:55:22 +0530

    Day3

---
+++ track/M03/T01/ST01/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Monday");
+        System.out.println("Tuesday wednesday");
+        System.out.println("Thursday");
+        System.out.println("friday saturday");
+        System.out.println("sunday");
+    }
+}
