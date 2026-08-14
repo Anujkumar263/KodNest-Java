011080cd55d9d13efd5b7148cb7aa650d0cd7106
Anuj Kumar <anujkumar263@gmail.com>
2026-08-14 14:46:50 +0530

    14/08/26

---
+++ track/M02/TO1/STO1/Class and object/Student141.java
@@ -0,0 +1,20 @@
+
+public class Student141 {
+
+    String name;
+    int age;
+    double hight;
+
+    void input(String name, int age, double hight) {
+        this.name = name;
+        this.age = age;
+        this.hight = hight;
+    }
+
+    void display() {
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(hight);
+    }
+
+}
---
+++ track/M02/TO1/STO1/Class and object/StudentApplication14.java
@@ -0,0 +1,15 @@
+
+public class StudentApplication14 {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        Student141 s1 = new Student141();
+        s1.input("Raja", 23, 5.5);
+        //s1.name="raja";
+        //s1.age=18;
+        //s1.hight=5.5;
+        s1.display();
+
+    }
+
+}
