6336824c2e9830a92e1e2b8313f80419d0d3ab76
Anuj Kumar <anujkumar263@gmail.com>
2026-08-17 15:23:51 +0530

    Array Demo

---
+++ track/M02/TO1/STO1/DataStructure/Demo1Array.java
@@ -0,0 +1,16 @@
+
+public class Demo1Array {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        int a[] = {10, 20, 30, 40, 50};
+
+        System.out.println(a[0]);
+        System.out.println(a[1]);
+        System.out.println(a[2]);
+        System.out.println(a[3]);
+        System.out.println(a[4]);
+
+    }
+
+}
