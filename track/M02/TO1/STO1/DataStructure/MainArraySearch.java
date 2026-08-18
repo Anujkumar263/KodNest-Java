3354a6ef3b73026f7bc79cf4ba6492966d38938f
Anuj Kumar <anujkumar263@gmail.com>
2026-08-18 15:53:08 +0530

    Array search

---
+++ track/M02/TO1/STO1/DataStructure/MainArraySearch.java
@@ -0,0 +1,12 @@
+
+public class MainArraySearch {
+
+    public static void main(String[] args) {
+
+        int[] arr = {10, 20, 30, 40, 50};
+
+        ArraySearch as = new ArraySearch();
+
+        as.search(arr, 30);
+    }
+}
