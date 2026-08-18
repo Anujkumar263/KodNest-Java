431a3b2577f704761265bdb613ea69113c3aaea1
Anuj Kumar <anujkumar263@gmail.com>
2026-08-18 15:13:24 +0530

    minimum nuber od element in Array

---
+++ track/M02/TO1/STO1/DataStructure/MinElementInArray.java
@@ -0,0 +1,46 @@
+
+import java.util.*;
+
+public class MinElementInArray {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        Scanner scan = new Scanner(System.in);
+
+        int arr[] = new int[5];
+
+        System.out.println("Enter Array Elements :");
+
+        for (int i = 0; i <= arr.length - 1; i++) {
+
+            arr[i] = scan.nextInt();
+
+        }
+
+        System.out.println("Array Elements are :");
+
+        for (int i = 0; i <= arr.length - 1; i++) {
+
+            System.out.print(arr[i] + " ");
+
+        }
+
+        System.out.println();
+
+        int min = arr[0];
+
+        for (int i = 0; i <= arr.length - 1; i++) {
+
+            if (arr[i] < min) {
+
+                min = arr[i];
+
+            }
+
+        }
+
+        System.out.println("Max element is : " + min);
+
+    }
+
+}
